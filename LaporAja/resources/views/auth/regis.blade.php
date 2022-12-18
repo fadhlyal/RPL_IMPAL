289c8f6c77dd9b3d362ddd63bbeadeb291967a75
riezaf51 <riezaf51@gmail.com>
2022-12-18 18:38:05 +0700

    ganti rasio foto

--- LaporAja/resources/views/auth/regis.blade.php
+++ LaporAja/resources/views/auth/regis.blade.php
@@ -86,10 +86,10 @@
                         <img src="images\loginimg.png" class="d-block w-100" alt="...">
                       </div>
                       <div class="carousel-item">
-                        <img src="images\loginimg.png" class="d-block w-100" alt="...">
+                        <img src="images\login2.jpg" class="d-block w-100" alt="...">
                       </div>
                       <div class="carousel-item">
-                        <img src="images\loginimg.png" class="d-block w-100" alt="...">
+                        <img src="images\login3.jpg" class="d-block w-100" alt="...">
                       </div>
                     </div>
                     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
